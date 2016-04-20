a1cc5d6066113aa03b2fd4c896e8b940a9878b2f
iandioch <iandioch11@gmail.com>
2016-04-20 23:11:07 +0100

    Add tweepy auth

--- server/tweetserver.py
+++ server/tweetserver.py
@@ -1,10 +1,22 @@
 from flask import Flask, request
+import tweepy
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
+api = None # tweepy api
 
-@app.route("/", methods=['POST'])
+@flask_app.route("/", methods=['POST'])
 def hello():
-    return request.form['tweet']
+    api.update_status(request.form['tweet'])
 
 if __name__ == '__main__':
-    app.run()
+    try:
+        auth_data = [l.strip() for l in open('twitter_auth.txt')]
+
+        auth = tweepy.OAuthHandler(auth_data[0], auth_data[1])
+        auth.set_access_token(auth_data[2], auth_data[3])
+        api = tweepy.API(auth)
+
+        flask_app.run()
+    except Exception as e:
+        print 'The server could not start:'
+        print e
